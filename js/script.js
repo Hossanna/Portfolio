ae1e2b11df68a03be7f854f3d054976c9e239c00
Hosanna Bivan <hosabivan@gmail.com>
2022-07-26 13:40:10 +0100

    nav

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var navMenu = document.getElementById("navMenu")
+var navbar = document.getElementById("navbar")
+
+function showMenu(){
+    navMenu.style.right = "0px"
+    navbar.classList.remove("sticky-top")
+}
+
+function hideMenu(){
+    navMenu.style.right= "-400px"
+}
